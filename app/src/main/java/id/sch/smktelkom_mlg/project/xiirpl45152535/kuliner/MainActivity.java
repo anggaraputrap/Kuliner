db31a362cda0d0c9c2b8f05b3e511eb1eead1122
sheila <sheilafatanihzasafitri@gmail.com>
2016-11-14 10:04:13 +0700

    penambahan script button

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl45152535/kuliner/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl45152535/kuliner/MainActivity.java
@@ -6,13 +6,54 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
 
 public class MainActivity extends ActionBarActivity {
+
+    private Button btn1,btn2,btn3,btn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = (Button) findViewById(R.id.buttonMenu);
+        btn2 = (Button)findViewById(R.id.buttonMap);
+        btn3 = (Button)findViewById(R.id.buttonInfo);
+        btn4 = (Button)findViewById(R.id.buttonExit);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah = new Intent(MainActivity.this,MenuActivity.class);
+                startActivity(pindah);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah = new Intent(MainActivity.this,MapActivity.class);
+                startActivity(pindah);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah = new Intent(MainActivity.this,VersiActivity.class);
+                startActivity(pindah);
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                moveTaskToBack(true);
+            }
+        });
 
 
         Intent intent = new Intent(this, LoginActivity.class);
---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl45152535/kuliner/MenuActivity.java
@@ -0,0 +1,15 @@
+package id.sch.smktelkom_mlg.project.xiirpl45152535.kuliner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        setTitle("Menu Makanan");
+    }
+}
